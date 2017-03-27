02164cd0587a3010a0d1f8c78a3698a66beb4aca
Warren Smith <warrens@uber.com>
2017-03-27 13:33:43 -0700

    Improve ResolvedBindings JavaDoc by linking BindingKey.

--- java/dagger/internal/codegen/ResolvedBindings.java
+++ java/dagger/internal/codegen/ResolvedBindings.java
@@ -35,8 +35,8 @@ import java.util.Optional;
 import java.util.Set;
 
 /**
- * The collection of bindings that have been resolved for a binding key. For valid graphs, contains
- * exactly one binding.
+ * The collection of bindings that have been resolved for a {@link BindingKey}. For valid graphs,
+ * contains exactly one binding.
  *
  * @author Gregory Kick
  */

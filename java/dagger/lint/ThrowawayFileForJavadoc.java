436ea9e9a1f3d1671f3f814f6c7001ae3c934725
Zac Sweers <zsweers@slack-corp.com>
2020-04-10 21:55:28 -0400

    Add throwaway for javadoc

---
+++ java/dagger/lint/ThrowawayFileForJavadoc.java
@@ -0,0 +1,7 @@
+package dagger.lint;
+
+/**
+ * Javadoc requires at least one java source file, but this project is kotlin-only. This exists to
+ * make that rule happy until KDoc/Dokka-only is supported.
+ */
+public enum ThrowawayFileForJavadoc {}
